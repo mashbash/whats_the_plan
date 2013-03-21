9f388390d3ee08f2cca7ae6ea27bba35316e65d6
Brayden Cleary <brayden.cleary@gmail.com>
2013-03-21 14:28:32 -0700

    first commit and configuration of database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatsThePlan::Application.config.secret_token = 'ad8b63ab1d8abc9216a54b759c0265d49324f0016c4ac70e11a2d955b332f9ddffa3d242d0e68985e932dceaa80cfc790e602ec648986d4b96b85adc6840f68c'
