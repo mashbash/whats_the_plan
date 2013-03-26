f09626470eb7ecf75a0ed3aba0fae1a0a5313c03
Justin Silverman <justin.silverman@gmail.com>
2013-03-25 22:15:16 -0700

    Update attribute list

--- app/models/yelp_result.rb
+++ app/models/yelp_result.rb
@@ -11,7 +11,7 @@ class YelpResult
     @state       = args["location"]["state_code"]
     @zip_code    = args["location"]["postal_code"]
     @country     = args["location"]["country_code"]
-    # @display_city   = args["location"]["display_address"].last
+    @display_city   = args["location"]["display_address"].last
     @latitude, @longitude = set_coordinates(args["location"]["coordinate"])
   end
 
