2741acfbd47fc8ed467b81de72bbd2a54de7842d
Justin Silverman <justin.silverman@gmail.com>
2013-03-26 11:50:08 -0700

    Add gmaps framework

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -12,8 +12,17 @@ module PlansHelper
     end
   end
 
-  def meal(index)  
+  def meal(index)
     (index % 2).zero? ? "Activity" : "Meal"
   end
 
+  def map(best_route)
+    waypoints = [best_route[1].gmaps_string, best_route[2].gmaps_string]
+    gmaps({ "direction" =>
+            { "data"      =>
+              { "from" => best_route.first.gmaps_string,
+                "to"   => best_route.last.gmaps_string},
+                "options" => {"travelMode" => "DRIVING", "waypoints" => waypoints} }
+          })
+  end
 end
