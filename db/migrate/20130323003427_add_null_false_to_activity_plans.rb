f316be8b1a675e3a06b75c7759e4e1830dd44e1f
Apprentice <apprentice@devbootcamp.com>
2013-03-26 09:15:50 -0700

    Delete unnecessary files

--- db/migrate/20130323003427_add_null_false_to_activity_plans.rb
+++
@@ -1,6 +0,0 @@
-class AddNullFalseToActivityPlans < ActiveRecord::Migration
-  def change
-    change_column_null :activity_plans, :activity_id, false
-    change_column_null :activity_plans, :plan_id, false
-  end
-end
--- db/migrate/20130323003917_add_timestamps_to_activity_plans.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToActivityPlans < ActiveRecord::Migration
-  def change
-    add_column :activity_plans, :created_at, :datetime, :null => false
-    add_column :activity_plans, :updated_at, :datetime, :null => false
-  end
-end
