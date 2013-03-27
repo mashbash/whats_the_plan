aa0f492e750e9efa16ac08ce06e87209ae4b91c8
Apprentice <apprentice@devbootcamp.com>
2013-03-27 09:52:53 -0700

    reverted sign up sign in to redirect to their pages and error messages appear in red

--- app/assets/javascripts/plan_creation.js
+++ app/assets/javascripts/plan_creation.js
@@ -8,23 +8,4 @@ jQuery(document).ready(function($) {
     var linkTo = $(this).find('td a').attr('href');
     window.location.href = linkTo;
   });
-
-  var matchLink = new RegExp(/sign_in|sign_up|users$/);
-  if (matchLink.test(window.location.pathname)) {
-    $('.show-signup').hide();
-    $('.show-signin').hide();
-  };
-
-  $('.show-signup').on('click', function(e){
-    e.preventDefault();
-    $('.signin-form').hide();
-    $('.signup-form').slideToggle();
-    console.log("i'm here! in signup!");
-  });
-
-  $('.show-signin').on('click', function(e){
-    e.preventDefault();
-    $('.signup-form').hide();
-    $('.signin-form').show();
-  });
 });
