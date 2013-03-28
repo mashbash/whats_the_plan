729b7a8467c321d729761ef7cab3de19aa5e874a
Justin Silverman <justin.silverman@gmail.com>
2013-03-28 11:43:27 -0700

    Refactor duplicative code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,4 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
-
-  def background_images
-    %w{ berlin singapore san\ francisco }.each do |destination|
-      
-    end
-    "<img id='#{name}'> src='/assets/#{name}.jpg'"
-  end
 end
